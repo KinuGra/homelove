04ea1ff3f7f18599fb58f192cd72d8a62c1a75c8
gear_kinu <kinusample@sample.kinu>
2025-03-20 22:03:20 +0900

    App.jsx, App.cssからcomponentsフォルダに分離

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import HamburgerMenu from './components/HamburgerMenu';
+import PraiseTextWindow from './components/PraiseTextWindow';
+import ImageDisplay from './components/ImageDisplay';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <div>
-          <img src="/public/img/tsukuyomi01.png" alt="Tsukuyomi" className="fullscreen-image" />
-          <div className="text-window">
-            <p>すごい！よく頑張ったね！</p>
-          </div>
-        </div>
-      </div>
-      <div className="actions-menu">
-        <button onClick={() => setMenuOpen(!menuOpen)} className={`btn btn--large btn--menu ${menuOpen ? 'open' : ''}`}>
-          {/* 3本線を削除 */}
-        </button>
-        {menuOpen && (
-          <div className="menu-buttons">
-            <button className="btn btn--share">Share</button>
-            <button className="btn btn--star">Star</button>
-            <button className="btn btn--comment">Comment</button>
-          </div>
-        )}
-      </div>
-    </>
-  )
+    <div>
+      <ImageDisplay />
+      <PraiseTextWindow />
+      <HamburgerMenu />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ImageDisplay.jsx
@@ -0,0 +1,9 @@
+import './ImageDisplay.css';
+
+function ImageDisplay() {
+  return (
+    <img src="/public/img/tsukuyomi01.png" alt="Tsukuyomi" className="fullscreen-image" />
+  );
+}
+
+export default ImageDisplay;
